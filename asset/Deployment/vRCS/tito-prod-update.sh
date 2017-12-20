55a73d7edaee09c51015f6a9c15957f91ba6530d
vmware <vmware@jumpbox.cpod-devops.shwrfr.mooo.com>
2017-12-20 11:57:37 +0100

    tito-prod-update added to automate prod update as part of  vRCS pipeline

---
+++ asset/Deployment/vRCS/tito-prod-update.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+#Created by Vince on the 19/12/17
+#Tito-Prod-update.sh
+#
+#
+
+
+#########initial setting
+TITO_RC_NEXT="tito-fe-rc-next.yml"
+TITO_RC_CURRENT="tito-fe-rc.yml"
+TITO_TEMP_DIR="/tmp/1861856966831705"
+TITO_TEMP_DIR+="/Tito/asset/Deployment/K8/"
+NEXT_LABEL="next"
+NEXT_NAME="titofe-next"
+TITO_VERSION="V0.5"
+
+echo "lancement du script update Prod Tito"
+echo -n "TITO_RC_NEXT="
+echo $TITO_RC_NEXT
+echo -n "TITO_RC_CURRENT="
+echo $TITO_RC_CURRENT
+echo -n "TITO_TEMP_DIR="
+echo $TITO_TEMP_DIR
+echo -n "NEXT LABEL="
+echo $NEXT_LABEL
+echo -n "NEXT_NAME="
+echo $NEXT_NAME
+echo -n "TITO_VERSION="
+echo $TITO_VERSION
+
+cd $TITO_TEMP_DIR
+cp $TITO_RC_CURRENT $TITO_RC_NEXT
+
+#change metadata.name
+sed -i "0,/name:.*/s//name: ${NEXT_NAME}/" $TITO_RC_NEXT
+
+#change version label at the rc selector level and at the container template level
+sed -i "/version:.*/s//version: ${NEXT_LABEL}/" $TITO_RC_NEXT
+
+#change version at the container level via the env variable
+sed -i "/value:.*/s//value: ${TITO_VERSION}/" $TITO_RC_NEXT
+
+#start rolling update
+kubectl -n=dev rolling-update titofe -f $TITO_RC_NEXT
