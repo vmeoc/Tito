9f82ab144bc553b2c773e97777f711a727c2c633
vmware <vmware@jumpbox.cpod-devops.shwrfr.mooo.com>
2017-12-20 15:29:01 +0100

    update

--- asset/Deployment/vRCS/cleanup.sh
+++ asset/Deployment/vRCS/cleanup.sh
@@ -11,14 +11,14 @@ TITO_RC_NEXT="tito-fe-rc-next.yml"
 TITO_TEMP_DIR="/tmp/"
 TITO_TEMP_DIR+=$1
 TITO_PROD_DIR="/home/vmware/Demo-do-not-touch/Tito/asset/Deployment/K8"
-TITO_RC="tito-fe-rc.ymltito-fe-rc.yml"
+TITO_RC="tito-fe-rc.yml"
 
 echo "lancement du script clean up"
 echo -n "TITO_TEMP_DIR="
 echo $TITO_TEMP_DIR
-echo -n "TITO_PROD_DIR"
+echo -n "TITO_PROD_DIR="
 echo $TITO_PROD_DIR
-echo -n "TITO_RC"
+echo -n "TITO_RC="
 echo $TITO_RC
 
 cd $TITO_PROD_DIR
