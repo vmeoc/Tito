9121a987c214b9eac65f41d32b5ffa9fa27e28dc
ahugla <ahugla@vmware.com>
2020-03-03 18:00:41 +0100

    Update index.php

--- index.php
+++ index.php
@@ -91,7 +91,7 @@ $(document).ready(function () {
             <div class="text-vertical-center">
                 <h1>Tito</h1>
                 <h3>Time Traffic Overview</h3>
-                <h5>Version: V2.3)</h5>
+                <h5>Version: V2.3</h5>
                 <h5>by Vince :)</h5>
                  <?PHP
                  echo "Runs on : " . gethostname() . " (" . getHostByName(getHostName()) .")";
