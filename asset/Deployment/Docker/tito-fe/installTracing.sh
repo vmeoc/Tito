ab6a85c55d6c45325fc36523acd7e10979dbecf2
ahugla <ahugla@vmware.com>
2020-03-05 11:32:56 +0100

    Decrease size and debugging

--- asset/Deployment/Docker/tito-fe/installTracing.sh
+++ asset/Deployment/Docker/tito-fe/installTracing.sh
@@ -2,45 +2,24 @@
 # Install:  Python 3.6, pip and SDK for wavefront tracing
 #
 # Alexandre hugla
-# 4 mars 2019
+# 5 mars 2019
 #
 # -----------------------------------------------------------
 
 
-# INSTALL PYTHON  (TRES LONG)
-#---------------
-cd /tmp
-
-apt-get update && sudo apt-get upgrade
-
-apt-get install -y do build-essential libssl-dev zlib1g-dev
-apt-get install -y libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm
-apt-get install -y libncurses5-dev libncursesw5-dev xz-utils tk-dev
-
-curl -O https://www.python.org/ftp/python/3.6.4/Python-3.6.4.tgz
-
-tar xvf Python-3.6.4.tgz
-
-#prepare the build
-cd Python-3.6.4
-#./configure --enable-optimizations     # LENT:  The configure option --enable-optimizations enables running test suites to generate data for profiling Python. The resulting python binary has better performance in executing python code. 
-./configure                             # PLUS RAPIDE
+# INSTALL PYTHON3 
+#----------------
+echo "deb http://ftp.de.debian.org/debian testing main" >> /etc/apt/sources.list
+echo 'APT::Default-Release "stable";' | tee -a /etc/apt/apt.conf.d/00local
+apt-get update
+apt-get -t testing install -y python3
 
 
-#build Python
-make -j8      # dure 30 min avec --enable-optimizations
-
-#install Python
-make altinstall
-
-python3.6 -V
-
-
-
-# INSTALL PYTHON-PIP
-#-------------------
+# INSTALL PYTHON3-PIP 
+#----------------------
+rm -f /etc/apt/apt.conf.d/00local
+apt-get update
 apt-get install -y python3-pip
-pip3 --version
 
 
 # INSTALL WAVEFRONT SDK 
