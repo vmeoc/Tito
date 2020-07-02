1f08660f21cc2db104834238c366ae8049af5e9e
root <root@vra-000844.cpod-vrealizesuite.az-demo.shwrfr.com>
2020-07-02 17:27:19 +0200

    added HTTPS for test with TF Cloud

--- asset/Deployment/Terraform/AWS/main.tf
+++ asset/Deployment/Terraform/AWS/main.tf
@@ -74,6 +74,14 @@ resource "aws_security_group" "default" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+ # HTTPS access from the VPC
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   # outbound internet access
   egress {
     from_port   = 0
